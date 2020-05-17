502d4799e32052103d7ba2543f825556b9bac260
Mandel <mandel.vaubourg@viacesi.fr>
2020-05-17 16:22:45 +0200

    Init

---
+++ binarytree.go
@@ -0,0 +1,104 @@
+package gomorse
+
+type Node struct {
+	Dot    *Node
+	Dash   *Node
+	Letter string
+}
+
+type Tree struct {
+	Groot *Node
+}
+
+func (node *Node) insert(code, letter string) {
+	if node == nil || len(code) != 1 {
+		return
+	}
+	if code == "." {
+		if node.Dot == nil {
+			node.Dot = &Node{Letter: letter}
+		} else {
+			node.Dot.insert(code, letter)
+		}
+	} else {
+		if node.Dash == nil {
+			node.Dash = &Node{Letter: letter}
+		} else {
+			node.Dash.insert(code, letter)
+		}
+	}
+}
+
+func (tree *Tree) search(code string) *Node {
+	currentNode := tree.Groot
+	for _, partialCode := range code {
+		pc := string(partialCode)
+
+		if pc == "." {
+			if currentNode.Dot == nil {
+				continue
+			}
+			currentNode = currentNode.Dot
+
+		} else {
+			if currentNode.Dash == nil {
+				continue
+			}
+			currentNode = currentNode.Dash
+
+		}
+	}
+	return currentNode
+}
+
+func (tree *Tree) insert(code, letter string) *Tree {
+	if tree.Groot == nil {
+		tree.Groot = &Node{Letter: letter, Dot: nil, Dash: nil}
+	} else {
+		if len(code) == 1 {
+			tree.Groot.insert(code, letter)
+		} else {
+			findedNode := tree.search(code)
+			findedNode.insert(code[len(code)-1:], letter)
+		}
+
+	}
+
+	return tree
+}
+
+func initTree() *Tree {
+	tree := &Tree{Groot: &Node{Letter: "ROOT"}}
+
+	tree.insert(".", "E").
+		insert(".-", "A").
+		insert(".-.", "R").
+		insert(".--", "W").
+		insert(".--.", "P").
+		insert(".---", "J").
+		insert(".-..", "L").
+		insert("..", "I").
+		insert("..-", "U").
+		insert("..-.", "F").
+		insert("...", "S").
+		insert("...-", "V").
+		insert("....", "H").
+		insert("-", "T").
+		insert("-.", "N").
+		insert("-.-", "K").
+		insert("-.--", "Y").
+		insert("-.-.", "C").
+		insert("-..", "D").
+		insert("-..-", "X").
+		insert("-...", "B").
+		insert("--", "M").
+		insert("--.", "G").
+		insert("--.-", "Q").
+		insert("--..", "Z").
+		insert("---", "O")
+
+	return tree
+}
+
+// MorseTree Binary tree that represent the morse language
+var MorseTree *Tree = initTree()
---
+++ binarytree_test.go
@@ -0,0 +1,39 @@
+package gomorse
+
+import "testing"
+
+func isValid(test *testing.T, tree *Tree, code, letter string) {
+	if tree.search(code).Letter != letter {
+		test.Error(code, letter, "not equal")
+	}
+}
+func TestEncode(test *testing.T) {
+
+	tree := MorseTree
+	isValid(test, tree, ".", "E")
+	isValid(test, tree, ".-", "A")
+	isValid(test, tree, ".-.", "R")
+	isValid(test, tree, ".--", "W")
+	isValid(test, tree, ".--.", "P")
+	isValid(test, tree, ".---", "J")
+	isValid(test, tree, ".-..", "L")
+	isValid(test, tree, "..", "I")
+	isValid(test, tree, "..-", "U")
+	isValid(test, tree, "..-.", "F")
+	isValid(test, tree, "...", "S")
+	isValid(test, tree, "...-", "V")
+	isValid(test, tree, "....", "H")
+	isValid(test, tree, "-", "T")
+	isValid(test, tree, "-.", "N")
+	isValid(test, tree, "-.-", "K")
+	isValid(test, tree, "-.--", "Y")
+	isValid(test, tree, "-.-.", "C")
+	isValid(test, tree, "-..", "D")
+	isValid(test, tree, "-..-", "X")
+	isValid(test, tree, "-...", "B")
+	isValid(test, tree, "--", "M")
+	isValid(test, tree, "--.", "G")
+	isValid(test, tree, "--.-", "Q")
+	isValid(test, tree, "--..", "Z")
+	isValid(test, tree, "---", "O")
+}
